1f7c72e2eb4208ef50ff6a464d051c1cf2bb1e8d
ipid <ipid@users.noreply.github.com>
2022-01-27 01:45:32 +0800

    Add `220126-r3birth-livestream`

---
+++ 220126-r3birth-livestream/encode.bat
@@ -0,0 +1,5 @@
+﻿
+@echo off
+chcp 65001
+
+vspipe video.vpy -c y4m - | ffmpeg -y -i pipe: -i "导出.mp4" -map 0:v -map 1:a -c:v libx264 -preset:v veryfast -crf:v 16 -qcomp:v 0.995 -c:a copy -shortest v2.mp4
