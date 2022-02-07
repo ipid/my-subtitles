6a870a3eeb916290ab97ec4f1ce11c33a69dd025
ipid <ipid@users.noreply.github.com>
2022-02-07 13:59:28 +0800

    Add multiple subtitles

--- 220126-r3birth-livestream/encode.bat
+++ 220126-r3birth-livestream/encode.bat
@@ -2,4 +2,4 @@
 @echo off
 chcp 65001
 
-vspipe video.vpy -c y4m - | ffmpeg -y -i pipe: -i "导出.mp4" -map 0:v -map 1:a -c:v libx264 -preset:v veryfast -crf:v 16 -qcomp:v 0.995 -c:a copy -shortest v2.mp4
+vspipe video.vpy -c y4m - | ffmpeg -y -i pipe: -i "导出.mp4" -map 0:v -map 1:a -c:v libx264 -preset:v veryfast -crf:v 16 -qcomp:v 0.995 -c:a copy -shortest v5.mp4
---
+++ 220204-ken-yorunaito/encode-海底捞.bat
@@ -0,0 +1,5 @@
+﻿
+@echo off
+chcp 65001
+
+vspipe video-海底捞.vpy -c y4m - | ffmpeg -y -i pipe: -i "【生肉】向健叔安利海底捞的鲤鱼姐.mp4" -map 0:v -map 1:a -c:v libx264 -preset:v veryfast -crf:v 16 -c:a libfdk_aac -b:a 320K -shortest 海底捞-v3.mp4
