d91328611f7a75863ca999ffee37ac91deb2009c
ipid <ipid@users.noreply.github.com>
2022-04-09 17:52:52 +0800

    A bunch of new subtitles

---
+++ 220325-lovelive-anng/encode-水渚鱼.bat
@@ -0,0 +1,9 @@
+﻿
+@echo off
+chcp 65001
+
+set VIDEO_PATH=导出-水渚鱼.mp4
+set SUBTITLE_PATH=字幕-水渚鱼.ass
+set OUT_FILENAME=水渚鱼-v1
+
+vspipe video.vpy -c y4m - | ffmpeg -y -i pipe: -i "%VIDEO_PATH%" -map 0:v -map 1:a -c:v libx264 -x264opts "bframes=0" -preset:v veryfast -crf:v 0 -g 5 -af aresample=async=99999 -c:a libfdk_aac -b:a 320K "%OUT_FILENAME%.mp4"
--- 220401-kaori-anige-eleven/encode-淤青.bat
+++ 220401-kaori-anige-eleven/encode-淤青.bat
@@ -4,6 +4,6 @@ chcp 65001
 
 set VIDEO_PATH=导出-淤青.mp4
 set SUBTITLE_PATH=字幕-淤青.ass
-set OUT_FILENAME=淤青-v1
+set OUT_FILENAME=淤青-v2
 
 vspipe video.vpy -c y4m - | ffmpeg -y -i pipe: -i "%VIDEO_PATH%" -map 0:v -map 1:a -c:v libx264 -preset:v veryfast -crf:v 16 -af aresample=async=99999 -c:a libfdk_aac -b:a 320K "%OUT_FILENAME%.mp4"
